28c369429977e9835959cc78c68b8388d9ba4ca1
AmalieJ <amaliecille@live.dk>
2022-05-19 20:45:55 +0200

    ja kollegaer

--- javascript/kollega.js
+++ javascript/kollega.js
@@ -1,4 +1,23 @@
-let kollegaListe = ["Hej", "med", "dig"];
+let kollegaListe = [
+    ["<div>" + "img/kollega-img.jpeg" + "<p>" + "Palle Henriksen" + "</p>" + "<p>" +"Programmør" + "</p>" + "</div>"],
+    ["<div>" + "img/kollega-img.jpeg" + "<p>" + "Palle Henriksen" + "</p>" + "<p>" +"Programmør" + "</p>" + "</div>"],
+    ["<div>" + "img/kollega-img.jpeg" + "<p>" + "Palle Henriksen" + "</p>" + "<p>" +"Programmør" + "</p>" + "</div>"],
+    ["<div>" + "img/kollega-img.jpeg" + "<p>" + "Palle Henriksen" + "</p>" + "<p>" +"Programmør" + "</p>" + "</div>"],
+    ["<div>" + "img/kollega-img.jpeg" + "<p>" + "Palle Henriksen" + "</p>" + "<p>" +"Programmør" + "</p>" + "</div>"],
+    ["<div>" + "img/kollega-img.jpeg" + "<p>" + "Palle Henriksen" + "</p>" + "<p>" +"Programmør" + "</p>" + "</div>"],
+    ["<div>" + "img/kollega-img.jpeg" + "<p>" + "Palle Henriksen" + "</p>" + "<p>" +"Programmør" + "</p>" + "</div>"],
+    ["<div>" + "img/kollega-img.jpeg" + "<p>" + "Palle Henriksen" + "</p>" + "<p>" +"Programmør" + "</p>" + "</div>"],
+];
 
-document.getElementsById("kollega-liste").innerHTML = kollegaListe;
+// loop the outer array
+for (let i = 0; i < kollegaListe.length; i++) {
+    document.write("<br>");
+    // loop the inner array
+    for (let j = 0; j < kollegaListe[i].length; j++) {
+        document.getElementById("kollega-liste").innerHTML += kollegaListe[i][j];
+    }
+}
 
+
+
+/*document.getElementById("kollega-liste").innerHTML = "<img src=\""+kollegaListe[0][0]+"\">";*/
