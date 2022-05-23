1807a5d732d0a0cc22d1d72246e5350602cb4422
Emilie <96144802+ebuchhansen@users.noreply.github.com>
2022-05-23 15:18:47 +0200

    javascript fix

--- javascript/login.js
+++ javascript/login.js
@@ -1,46 +1,7 @@
-let number = 1;
-
-if (number > 0){
-    console.log("ifWorking");
-}
-else {
-    console.log("elseWorking");
-}
-
-let ordo = document.getElementById("ordo");
-document.getElementById("ordo").innerHTML = "Ordo";
-
-
-const kodeord = {
-    firstName: "Glemt",
-    lastName: " kodeord",
-    fullName: function() {
-      return this.firstName + this.lastName;
-    }
-};
-document.getElementById("opret").innerHTML = kodeord.fullName();
-
-
-const bruger = ["Opret", " bruger"];
-
-document.getElementById("opret2").innerHTML =
-bruger[0] + bruger[1];
-
-
-let text = "";
-let i = 0;
-while (i < 1) {
-    text += "<h2>Login</h2>";
-    i++;
-}
-document.getElementById("logintitle").innerHTML = text;
-
-
-
 let objPeople = [
 	{
 		username: "amalie",
-		password: "jorgensen"
+		password: "jensen"
 	},
 	{
 		username: "andrea",
