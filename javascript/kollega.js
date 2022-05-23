50c17bee966c664f3b9733b6d43edda0967825b2
AmalieJ <amaliecille@live.dk>
2022-05-23 22:37:45 +0200

    style guide

--- javascript/kollega.js
+++ javascript/kollega.js
@@ -14,11 +14,10 @@ for (let i = 0; i < kollegaListe.length; i++) {
     // loop the inner array
     for (let j = 0; j < kollegaListe[i].length; j++) {
       if (j==1){
-        document.getElementById("kollega-liste").innerHTML += "<img class='kollega-billeder' src='" + kollegaListe[i][j] + "'>";
+        document.getElementById("kollega-liste").innerHTML += "<img src='" + kollegaListe[i][j] + "'>";
       } else {
         document.getElementById("kollega-liste").innerHTML += kollegaListe[i][j];
       }
     }
 }
 
-/*document.getElementById("kollega-liste").innerHTML = "<img src=\""+kollegaListe[0][0]+"\">";*/
