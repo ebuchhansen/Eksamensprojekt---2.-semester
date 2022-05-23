bc48c1f730371b3554fa6a282ca680b55c01e486
Emilie <96144802+ebuchhansen@users.noreply.github.com>
2022-05-23 15:10:16 +0200

    mit javascript ny

--- javascript/login.js
+++ javascript/login.js
@@ -34,3 +34,42 @@ while (i < 1) {
     i++;
 }
 document.getElementById("logintitle").innerHTML = text;
+
+
+
+let objPeople = [
+	{
+		username: "amalie",
+		password: "jorgensen"
+	},
+	{
+		username: "andrea",
+		password: "lyng"
+	},
+	{
+		username: "julie",
+		password: "bekker"
+	},
+    {
+        username: "emilie",
+        password: "hansen"
+    }
+
+];
+
+function getInfo() {
+	let username = document.getElementById('username').value;
+	let password = document.getElementById('password').value;
+
+	for(let i = 0; i < objPeople.length; i++) {
+
+		if(username == objPeople[i].username && password == objPeople[i].password) {
+			console.log(username + " er logget ind");
+
+            location.href = "https://www.w3schools.com";
+
+			return;
+		}
+	}
+	console.log("forkert brugernavn eller kodeord");
+}
