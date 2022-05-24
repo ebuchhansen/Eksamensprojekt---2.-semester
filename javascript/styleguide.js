f050cc2edc8a31ac43ba50095c073816f3ac35fa
AmalieJ <amaliecille@live.dk>
2022-05-24 15:13:16 +0200

    style guide

--- javascript/styleguide.js
+++ javascript/styleguide.js
@@ -16,10 +16,10 @@ window.onclick = function(event) {
 }
 
 
- let farveArray = ["#FCFAF0", "#DCBCFD", "#36244D", "#4E3B66", "#CBEBDB", "#F2D7CA"];
+let farveArray = ["#FCFAF0", "#DCBCFD", "#36244D", "#4E3B66", "#CBEBDB", "#F2D7CA"];
 
- for (let i = 0; i < farveArray.length; i++) {
+for (let i = 0; i < farveArray.length; i++) {
   document.getElementById("farve-array").innerHTML += "<p>" + farveArray[i] + "</p>";
- }
+}
 
 
